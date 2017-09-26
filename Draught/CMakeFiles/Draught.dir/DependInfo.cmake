8642da5253c6c1ca88f67578deee193b10597614
qLience <32282946+qLience@users.noreply.github.com>
2017-09-26 10:37:34 +1000

    Add files via upload

---
+++ Draught/CMakeFiles/Draught.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sh6/S3655676/Draught/src/main.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/main.cpp.o"
+  "/home/sh6/S3655676/Draught/src/model/model.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/model/model.cpp.o"
+  "/home/sh6/S3655676/Draught/src/nc_controller/command.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/nc_controller/command.cpp.o"
+  "/home/sh6/S3655676/Draught/src/nc_controller/controller.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/nc_controller/controller.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/add_player_window.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/add_player_window.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/game_window.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/game_window.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/main_menu.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/main_menu.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/menu.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/menu.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/player_selection_window.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/player_selection_window.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/ui.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/ui.cpp.o"
+  "/home/sh6/S3655676/Draught/src/ncview/window.cpp" "/home/sh6/S3655676/Draught/CMakeFiles/Draught.dir/src/ncview/window.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Draught/CMakeFiles/Draught.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Draught.dir/src/main.cpp.o"
+  "CMakeFiles/Draught.dir/src/model/model.cpp.o"
+  "CMakeFiles/Draught.dir/src/nc_controller/command.cpp.o"
+  "CMakeFiles/Draught.dir/src/nc_controller/controller.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/add_player_window.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/game_window.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/main_menu.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/menu.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/player_selection_window.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/ui.cpp.o"
+  "CMakeFiles/Draught.dir/src/ncview/window.cpp.o"
+  "Draught.pdb"
+  "Draught"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Draught.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
