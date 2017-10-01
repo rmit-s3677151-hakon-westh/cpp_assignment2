4daa3998d091ce61bec3c1482bb71c2031343087
rmit-s3677151-hakon-westh <s3677151@student.rmit.edu.au>
2017-10-01 18:38:52 +1100

    NU SKAL DET FUNGE JEG GIDER IKKE AT FIKSE DET IGEN

--- model.cpp
+++ model.cpp
@@ -23,20 +23,90 @@ int draughts::model::model::get_player_score(int playernum)
 
 void draughts::model::model::start_game(int plr1, int plr2)
 {
-	// Validate players are in roster
+    // Validate players are in roster
 	if(!draughts::model::model::player_exists(plr1) ||
 		!draughts::model::model::player_exists(plr2))
 	{
 		if(!draughts::model::model::player_exists(plr1)){
 			std::cerr << plr1 << " ID do not exist in the roster" << std::endl;
 		}
-		else{
+		if(!draughts::model::model::player_exists(plr2)){
 			std::cerr << plr2 << " ID do not exist in the roster" << std::endl;
 		}
+	return;
 	}
-	
-	
+
+	// Create tokens
+	draughts::model::model::create_tokens(1,plr1);
+	draughts::model::model::create_tokens(2,plr2);
+
+	// Initialise board by placing tokens on board
+	draughts::model::model::initialise_board();
+
+	// Initialise that player 1 will start game
+	turn = true; // TRUE player 1 and FALSE player 2
 }
+
+
+void draughts::model::model::create_tokens(int player_in_game, int plrID)
+{
+	// Set token
+	char token;
+	if(player_in_game == 1){
+		token = 'x';
+	}
+	else{
+		token = 'o';
+	}
+
+	// Create pieces
+	for (int piece = 0; piece < 12; ++piece){
+		if(player_in_game == 1){
+			player1_pieces.push_back(draughts::model::piece(plrID, token));
+		}
+		else{
+			player2_pieces.push_back(draughts::model::piece(plrID, token));
+		}
+	}
+}
+
+void draughts::model::model::initialise_board(){
+
+	// Player 1
+		// Row 1
+		player1_pieces[0].set_positionXY(std::make_pair(1,2));
+		player1_pieces[1].set_positionXY(std::make_pair(1,4));
+		player1_pieces[2].set_positionXY(std::make_pair(1,6));
+		player1_pieces[3].set_positionXY(std::make_pair(1,8));
+		// Row 2
+		player1_pieces[4].set_positionXY(std::make_pair(2,1));
+		player1_pieces[5].set_positionXY(std::make_pair(1,3));
+		player1_pieces[6].set_positionXY(std::make_pair(1,5));
+		player1_pieces[7].set_positionXY(std::make_pair(1,7));
+		// Row 3
+		player1_pieces[8].set_positionXY(std::make_pair(3,2));
+		player1_pieces[9].set_positionXY(std::make_pair(3,4));
+		player1_pieces[10].set_positionXY(std::make_pair(3,6));
+		player1_pieces[11].set_positionXY(std::make_pair(3,8));
+
+	// Player 2
+		// Row 6
+		player2_pieces[0].set_positionXY(std::make_pair(6,1));
+		player2_pieces[1].set_positionXY(std::make_pair(6,3));
+		player2_pieces[2].set_positionXY(std::make_pair(6,5));
+		player2_pieces[3].set_positionXY(std::make_pair(6,7));
+		// Row 7
+		player2_pieces[4].set_positionXY(std::make_pair(7,2));
+		player2_pieces[5].set_positionXY(std::make_pair(7,4));
+		player2_pieces[6].set_positionXY(std::make_pair(7,6));
+		player2_pieces[7].set_positionXY(std::make_pair(7,8));
+		// Row 8
+		player2_pieces[8].set_positionXY(std::make_pair(8,1));
+		player2_pieces[9].set_positionXY(std::make_pair(8,3));
+		player2_pieces[10].set_positionXY(std::make_pair(8,5));
+		player2_pieces[11].set_positionXY(std::make_pair(8,7));
+}
+
 
 int draughts::model::model::get_winner()
 {
@@ -45,6 +115,11 @@ int draughts::model::model::get_winner()
 
 std::string draughts::model::model::get_player_name(int id)
 {
+    for (auto p_it = player_vector.begin(); p_it != player_vector.end(); ++p_it)
+    {
+        if (id == (*p_it).get_player_ID())
+            return (*p_it).get_player_name();
+    }
     return "";
 }
 
@@ -53,9 +128,39 @@ char draughts::model::model::get_token(int x ,int y)
     return '\0';
 }
 
+bool draughts::model::model::validate_move(int playernum,
+    int startx, int starty, int endx, int endy)
+{
+    auto p_ptr = draughts::model::model::get_piece_from_position(startx, starty);
+    if ((*p_ptr).get_ownerID() != playernum)
+        return false;
+    /* TODO: insert rules here */
+    return true;
+}
+
+std::unique_ptr<draughts::model::piece> draughts::model::model::get_piece_from_position(int pos_x, int pos_y)
+{
+    std::unique_ptr<draughts::model::piece> p_ptr;
+    auto input_positionXY = std::make_pair(pos_x, pos_y);
+    for (auto p1_it = player1_pieces.begin(); p1_it != player1_pieces.end(); ++p1_it)
+    {
+        /* TODO should we do something about the pointer to pointer shit??? */
+        if (input_positionXY == (*p1_it).get_positionXY())
+            p_ptr = std::make_unique<draughts::model::piece>(*p1_it);
+    }
+    for (auto p2_it = player2_pieces.begin(); p2_it != player2_pieces.end(); ++p2_it)
+    {
+        if (input_positionXY == (*p2_it).get_positionXY())
+            p_ptr = std::make_unique<draughts::model::piece>(*p2_it);
+    }
+    return p_ptr;
+}
+
 void draughts::model::model::make_move(int playernum,
         int startx, int starty, int endx, int endy)
 {
+    auto p_ptr = draughts::model::model::get_piece_from_position(startx, starty);
+    (*p_ptr).set_positionXY(make_pair(endx, endy));
 }
 
 void draughts::model::model::add_player(const std::string& p)
@@ -94,7 +199,10 @@ bool draughts::model::model::player_exists(int pID)
 
 int draughts::model::model::get_current_player(void)
 {
-    return EOF;
+    if (turn)
+        return (*player_1).get_player_ID();
+    else
+        return (*player_2).get_player_ID();
 }
 
 std::map<int, std::string> draughts::model::model::get_player_list(void)
--- model.h
+++ model.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <algorithm>
 #include "player.h"
+#include "piece.h"
 
 #pragma once
 
@@ -22,22 +23,26 @@ namespace draughts
                 static std::unique_ptr<model> instance;
                 model(void);
                 bool player_exists(const std::string&); /* DONE */
-				bool player_exists(int);
-                /*player player1;
-                player player2;*/
-                /* vector<unique_ptr<piece>> */
+                std::unique_ptr<player> player1;
+                std::unique_ptr<player> player2;
+                std::vector<piece> player1_pieces;
+                std::vector<piece> player2_pieces;
                 std::vector<player> player_vector;
-				int width = 8;
-				int height = 8;
+                bool turn;
+                const int WIDTH = 8;
+                const int HEIGHT = 8;
             public:
                 void start_game(int, int); /* Søren */
+                void create_tokens(int, int); /* Søren */
+                void initialise_board(); /* Søren */
                 char get_token(int,int); /* Søren */
-                //bool validate_move(piece& moving_piece); /* Håkon */
-                void make_move(int, int, int, int, int); /* Håkon */
+                bool validate_move(int, int, int, int, int); /* Håkon */
+                std::unique_ptr<piece> get_piece_from_position(int, int); /* DONE */
+                void make_move(int, int, int, int, int); /* DONE */
                 void add_player(const std::string&); /* DONE */
                 int get_player_score(int); /* Søren */
-                int get_current_player(void); /* Håkon */
-                std::string get_player_name(int); /* Håkon */
+                int get_current_player(void); /* DONE */
+                std::string get_player_name(int); /* DONE */
                 std::map<int, std::string> get_player_list(void) const; /* DONE */
                 int get_winner(); /* Håkon */
                 int get_width(); /* Søren */
